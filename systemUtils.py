13f92948eaa4e14aca6e771a717e03a172c5a066
pnavneet <navneetpurohit1@gmail.com>
2024-04-13 22:31:38 -0700

    defining functions to get system details

--- systemUtils.py
+++ systemUtils.py
@@ -8,12 +8,12 @@ import psutil
 class SystemUtils(object):
 
     def __init__(self):
-        self.host_name = None
-        self.host_platform = None
-        self.host_vendor = None
-        self.host_os = None
-        self.host_cpu = None
-        self.host_memory = None
+        self.system_name = None
+        self.system_platform = None
+        self.system_vendor = None
+        self.system_os = None
+        self.system_cpu = None
+        self.system_memory = None
 
 
     def _run_cmd(self, cmd):
@@ -21,19 +21,44 @@ class SystemUtils(object):
         pass
 
     def _get_system_name(self):
-        self.host_name = platform.node()
-        return self.host_name
+        self.system_name = platform.node()
+        return self.system_name
     
     def _get_system_platform(self):
-        self.host_platform = platform.platform()
-        return self.host_platform
+        self.system_platform = platform.platform()
+        return self.system_platform
     
     def _get_system_vendor(self):
-        pass
+        self.system_vendor = "TBD"
+
+    def _get_system_cpu(self):
+        self.system_cpu = "TBD"
+
+    def _get_system_os(self):
+        self.system_os = "TBD"
+
+    def _get_system_memory(self):
+        self.system_memory = "TBD"
 
 
     def _display_system_info(self):
         """ Function to display system info"""
-        print("")
+        print("*"*30)
+        print("{:>20}".format("Displaying System Info"))
+        print("*"*30)
+        print("- {:<20}: {}".format("System Name", self.system_name))
+        print("- {:<20}: {}".format("System Vendor", self.system_vendor))
+        print("- {:<20}: {}".format("System Platform", self.system_platform))
+        print("- {:<20}: {}".format("System OS", self.system_os))
+        print("- {:<20}: {}".format("System CPU", self.system_cpu))
+        print("- {:<20}: {}".format("System RAM", self.system_memory))
 
     
+    def get_system_details(self):
+        self._get_system_name()
+        self._get_system_vendor()
+        self._get_system_platform()
+        self._get_system_os()
+        self._get_system_cpu()
+        self._get_system_memory()
+

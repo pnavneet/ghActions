ca01b992c94b1f58f7b58d0fe8742855e05086c5
pnavneet <navneetpurohit1@gmail.com>
2024-04-13 22:38:17 -0700

    test script to get and display system details

---
+++ get_system_details.py
@@ -0,0 +1,17 @@
+# Script to get system details
+
+from systemUtils import SystemUtils
+
+class TestSystem(object):
+
+    def __init__(self):
+        self.systemObj = SystemUtils()
+
+    def run_system_test(self):
+        self.systemObj.get_system_details()
+        self.systemObj.display_system_info()
+
+
+if __name__ == '__main__':
+    obj = TestSystem()
+    obj.run_system_test()
